514f79e2a7455f02f523b27459a645fe617e60c8
TomBuk4 <104893605+TomBuk4@users.noreply.github.com>
2022-10-13 08:30:55 +0200

    Update Checker.cs

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._11
 {
@@ -6,7 +6,15 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
-        } 
+            string first = str.Substring(0, str.Length / 2);
+            char[] a = str.ToCharArray();
+
+            Array.Reverse(a);
+
+            string reverse = new string(a);
+            string second = reverse.Substring(0, reverse.Length / 2);
+
+            return str == reverse;
+        }
     }
 }
